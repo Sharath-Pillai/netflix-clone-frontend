6c916d52decfd716a3eaf5c2e7afaa76e9148e69
sharath pillai <spillai.developer@gmail.com>
2026-03-25 14:24:20 +0530

    banner image added into asset folder + banner.jsx & banner.css modified

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,25 @@
-import React,{useState} from 'react'
-import './Banner.css'
-const Banner= ()=> {
-    const[movie,setMovie]=useState('')
-    
-  return (
-    
-    <div  className='banner'>
-        <div className="content">
-            <h1 className="title">
-            </h1>
-            <div className="banner_buttons">
-                <button className="button">Play</button>
-                <button className="button">My List</button>
-            </div>
-            <p className="description">
-            </p>
-        </div>
-        <div className="fade_bottom"></div>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import "./Banner.css";
+const Banner = () => {
+  const [movie, setMovie] = useState("");
 
-export default Banner
+  return (
+    <div className="banner">
+      <div className="content">
+        <h1 className="title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
+        </div>
+        <p className="description">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+          laborum nobis officiis laboriosam nisi doloremque eaque veritatis
+          libero dicta a.
+        </p>
+      </div>
+      <div className="fade_bottom"></div>
+    </div>
+  );
+};
+
+export default Banner;
