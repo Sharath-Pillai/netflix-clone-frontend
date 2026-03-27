2efe7925fea04b1c2868d1bb9876a7cc534382ea
sharath pillai <spillai.developer@gmail.com>
2026-03-27 19:44:05 +0530

    urls created the api url links get from website, api key already get from site and stored in constans

---
+++ src/urls.js
@@ -0,0 +1,17 @@
+import { API_KEY } from './Constants/constants';
+
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
+
+export const trending = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`;
+
+export const originals = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`;
+
+export const action = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`;
+
+export const comedy = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`;
+
+export const horror = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`;
+
+export const romance = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`;
+
+export const documentaries = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`;
