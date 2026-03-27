6c7f318baeb804d18d2c03c74b6abb2f05d7fd8b
sharath pillai <spillai.developer@gmail.com>
2026-03-27 20:06:58 +0530

    scroll state craeted+ useeffect added and handlescroll,requestAnimationFrame,window.scrolly also used. at the end scroll event addlstner removed to prevent duplicate listner and memoryleak

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import './Navbar.css'
-const Navbar=()=> {
-  return (
-    <div className='navbar'>
-        <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix-logo" />
-        <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Netflix-avatar" />
-    </div>
-  )
-}
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
 
-export default Navbar
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    let ticking = false;
+
+    const handleScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          setScrolled(window.scrollY > 50);
+          ticking = false;
+        });
+        ticking = true;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <nav className={`navbar ${scrolled ? 'navbar--scrolled' : ''}`}>
+      <div className="navbar__left">
+        <img
+          className="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+          alt="Netflix logo"
+        />
+        <ul className="nav__links">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>My List</li>
+        </ul>
+      </div>
+      <img
+        className="avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="User avatar"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
