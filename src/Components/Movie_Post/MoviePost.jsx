5f1d640610c70c116c67a53dde8b04786c8f32fc
sharath pillai <spillai.developer@gmail.com>
2026-03-28 13:39:12 +0530

    moviepost.css modified and in moviepost.jsx file usenavigae,mylistcontex imported then onclik to image poster handle play fn wroks and  it will navigate to player page and video will visible there.  handlelisttoggle fn for add or remove from movielist with the help of addtolist,removfromlist check with isinlist

--- src/Components/Movie_Post/MoviePost.jsx
+++ src/Components/Movie_Post/MoviePost.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "./moviePost.css";
 import axios from "axios";
 import { imageUrl } from "../../Constants/constants";
+import { MyListContext } from "../../Context/MyListContext";
 
 const MoviePost = ({ url, title, isSmall }) => {
   const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
+  const { addToList, removeFromList, isInList } = useContext(MyListContext);
 
   useEffect(() => {
     async function fetchMovies() {
@@ -18,19 +22,43 @@ const MoviePost = ({ url, title, isSmall }) => {
     if (url) fetchMovies();
   }, [url]);
 
+  const handlePlay = (movie) => {
+    const movieTitle = movie?.title || movie?.name || movie?.original_name;
+    if (movieTitle) {
+      navigate(`/player/${encodeURIComponent(movieTitle)}`);
+    }
+  };
+
+  const handleListToggle = (e, movie) => {
+    e.stopPropagation();
+    if (isInList(movie.id)) {
+      removeFromList(movie.id);
+    } else {
+      addToList(movie);
+    }
+  };
+
   return (
     <div className="row">
       <h2 className="row__title">{title}</h2>
       <div className="posters">
         {movies.map((movie) =>
           movie.poster_path ? (
-            <img
-              key={movie.id}
-              className={isSmall ? "poster small_poster" : "poster"}
-              src={`${imageUrl}${movie.poster_path}`}
-              alt={movie.title || movie.name}
-              title={movie.title || movie.name}
-            />
+            <div key={movie.id} className="poster__container">
+              <img
+                onClick={() => handlePlay(movie)}
+                className={isSmall ? "poster small_poster" : "poster"}
+                src={`${imageUrl}${movie.poster_path}`}
+                alt={movie.title || movie.name}
+                title={movie.title || movie.name}
+              />
+              <button
+                className="poster__listBtn"
+                onClick={(e) => handleListToggle(e, movie)}
+              >
+                {isInList(movie.id) ? "✓" : "+"}
+              </button>
+            </div>
           ) : null
         )}
       </div>
