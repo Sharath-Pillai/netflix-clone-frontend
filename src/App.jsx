b72f913875ee272ad40836149a46bf70147b9f23
sharath pillai <spillai.developer@gmail.com>
2026-03-28 09:17:08 +0530

    navbar,banner and moviepost componets added in app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Banner from "./Components/Banner/Banner";
 import MoviePost from "./Components/Movie_Post/MoviePost";
+import {
+  originals,
+  trending,
+  action,
+  comedy,
+  horror,
+  romance,
+  documentaries,
+} from "./urls";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Banner />
-      <MoviePost/>
+      <MoviePost url={originals} title="Netflix Originals" />
+      <MoviePost url={trending} title="Trending Now" isSmall />
+      <MoviePost url={action} title="Action Movies" isSmall />
+      <MoviePost url={comedy} title="Comedy Movies" isSmall />
+      <MoviePost url={horror} title="Horror Movies" isSmall />
+      <MoviePost url={romance} title="Romance Movies" isSmall />
+      <MoviePost url={documentaries} title="Documentaries" isSmall />
     </div>
   );
 };
